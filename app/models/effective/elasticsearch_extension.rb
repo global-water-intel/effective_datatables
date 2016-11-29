f1126333989ce93973e00de49b8f94dcba56cb4b
Sebastian Iorga <si@globalwaterintel.com>
2016-11-29 06:47:52 +0000

    allow additional index namespacing

--- app/models/effective/elasticsearch_extension.rb
+++ app/models/effective/elasticsearch_extension.rb
@@ -8,7 +8,7 @@ module Effective
 
     module ClassMethods
       def elasticsearch_initialize
-        index_name "#{Rails.application.class.parent.to_s.underscore}_#{table_name}_#{Rails.env}".downcase
+        index_name "#{Rails.application.class.parent.to_s.underscore}_#{table_name}_#{Rails.env}#{Rails.configuration.elasticsearch_suffix}".downcase
         settings_options = {
           index: {
             number_of_shards: 1,
